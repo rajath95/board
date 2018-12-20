d8a58556353009fe960541c7203273476928968c
rajath95 <simplytalk95@gmail.com>
2018-12-20 17:36:02 +0530

    edited views.py

--- board/board/views.py
+++ board/board/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,render_to_response
 
 
 def basic(request):
-    return HttpResponse("Welcome")
+    return render_to_response('index.html',context=None)
 
 def table(request):
     return render_to_response('table.html',context=None)
